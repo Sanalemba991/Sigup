6f88e17fdbae97626aa89ffe455f792e50be11fc
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2024-11-07 21:37:35 +0530

    connected

--- server/index.js
+++ server/index.js
@@ -1,15 +1,20 @@
 const express = require("express");
 const bcrypt = require("bcryptjs");
 const mongoose = require("mongoose");
-const dotenv = require("dotencv");
+const dotenv = require("dotenv");
 const cors = require("cors");
 
-dotenv.config();
+dotenv.config();  
 
 const app = express();
 app.use(express.json());
-app.use(cors())
+app.use(cors()); 
 
-mongoose.connect(MONGI_URI).then(()=>console.log("Connected to MONGODB"))
-.catch(err =>console.log("Connected to MONGODB",err))
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch(err => console.log("Error connecting to MongoDB:", err));
 
+// Start the server
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
