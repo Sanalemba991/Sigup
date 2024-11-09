326505487ed2b628576573e218363b0c35defb0b
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2024-11-09 16:31:04 +0530

    Home Created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import SignUp from "./SignUp";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LogIn from "./LogIn";
+import Home from "./Home";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path='/register' element={<SignUp/>}></Route>
           <Route path='/login' element={<LogIn/>}></Route>
+          <Route path='/home' element={<Home/>}></Route>
         </Routes>
       </BrowserRouter>
     </>
