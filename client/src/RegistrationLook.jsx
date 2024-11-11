0d6bd8a0e1e740450029cc6b9859a48acd734d32
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2024-11-11 21:32:39 +0530

    making

--- client/src/RegistrationLook.jsx
+++ client/src/RegistrationLook.jsx
@@ -1,9 +1,18 @@
-import React, { useState } from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
 function RegistrationLook() {
     const[data,setData]=useState("")
+
+useEffect(()=>{
+    axios.get("http://localhost:4000/reservation")
+    .then((respons)=>{
+        data
+    })
+})
   return (
-    <div>
+    <div className=''>
+        
       
     </div>
   )
