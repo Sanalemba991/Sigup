526f3faff4e03ed63f9a8d96e2897e1bd14887a3
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2024-11-11 21:43:51 +0530

    Making Good

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,7 +15,7 @@ function App() {
           <Route path='/' element={<LogIn/>}></Route>
           <Route path='/home' element={<Home/>}></Route>
           <Route path="/success" element={<Success/>}/>
-          
+          <Route path="/reg" element={<RegistrationLook/>}/>
           <Route path="*" element={<NotFound/>}/>
         </Routes>
       </BrowserRouter>
