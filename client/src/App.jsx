c635727491f6f88cf03e9d8bb456f2888aa7c490
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2024-11-12 19:03:40 +0530

    ok

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ import LogIn from "./LogIn";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import Success from "./pages/Success"
-import RegistrationLook from "./RegistrationLook";
+
 function App() {
   return (
     <>
@@ -15,7 +15,7 @@ function App() {
           <Route path='/' element={<LogIn/>}></Route>
           <Route path='/home' element={<Home/>}></Route>
           <Route path="/success" element={<Success/>}/>
-          <Route path="/reg" element={<RegistrationLook/>}/>
+       
           <Route path="*" element={<NotFound/>}/>
         </Routes>
       </BrowserRouter>
